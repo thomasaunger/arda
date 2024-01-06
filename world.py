28182c9c275c63954086af8fa0752a422a81e56b
Thomas Unger <thomas.a.unger@icloud.com>
2024-01-06 19:19:25 +0100

    Rename "change" variable to "action"

--- world.py
+++ world.py
@@ -35,37 +35,37 @@ while running:
         ], dtype=np.int8
     ) for agent_id in range(world.num_agents)}
 
-    change = False
+    action = False
 
     t_delta = 0.1
 
     if keys[pg.K_UP] and (not prev_keys[pg.K_UP] or (time.time() - t_press > t_delta and not world.first_action[PLAYER_A] and world.last_move_legal[PLAYER_A])):
         print("UP")
         actions[PLAYER_A][Realm.MOVE] = Realm.actions[Realm.MOVE]["FORWARD"]
-        change = True
+        action = True
     elif keys[pg.K_RIGHT] and (not prev_keys[pg.K_RIGHT] or (time.time() - t_press > t_delta and not world.first_action[PLAYER_A])):
         print("RIGHT")
         actions[PLAYER_A][Realm.TURN] = Realm.actions[Realm.TURN]["RIGHT"]
-        change = True
+        action = True
     elif keys[pg.K_LEFT] and (not prev_keys[pg.K_LEFT] or (time.time() - t_press > t_delta and not world.first_action[PLAYER_A])):
         print("LEFT")
         actions[PLAYER_A][Realm.TURN] = Realm.actions[Realm.TURN]["LEFT"]
-        change = True
+        action = True
     
     if keys[pg.K_i] and (not prev_keys[pg.K_i] or (time.time() - t_press > t_delta and not world.first_action[PLAYER_B] and world.last_move_legal[PLAYER_B])):
         print("i")
         actions[PLAYER_B][Realm.MOVE] = Realm.actions[Realm.MOVE]["FORWARD"]
-        change = True
+        action = True
     elif keys[pg.K_l] and (not prev_keys[pg.K_l] or (time.time() - t_press > t_delta and not world.first_action[PLAYER_B])):
         print("l")
         actions[PLAYER_B][Realm.TURN] = Realm.actions[Realm.TURN]["RIGHT"]
-        change = True
+        action = True
     elif keys[pg.K_j] and (not prev_keys[pg.K_j] or (time.time() - t_press > t_delta and not world.first_action[PLAYER_B])):
         print("j")
         actions[PLAYER_B][Realm.TURN] = Realm.actions[Realm.TURN]["LEFT"]
-        change = True
+        action = True
 
-    if change:
+    if action:
         t_press = time.time()
         _, _, done, _ = world.step(actions)
         if done:
