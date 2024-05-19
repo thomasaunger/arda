2a2e89573700de883a06a45711dec17d534cbc7a
Thomas Unger <thomas.a.unger@icloud.com>
2024-05-19 20:25:30 +0200

    FIx action conditionals

--- realm/spaces/utils/regular_tiling.py
+++ realm/spaces/utils/regular_tiling.py
@@ -91,16 +91,24 @@ class RegularTiling(Space):
             )
             self.array[tuple(self.agent_points[i_movers].T)] = i_movers + 2
 
-        if 0 < (i_lefters := np.where(
-            actions.T[self.action_space.MOVE] == self.action_space.MOVE.NONE and
-            actions.T[self.action_space.TURN] == self.action_space.TURN.LEFT
-        )[0]).size:
+        if 0 < (i_lefters := np.intersect1d(
+                np.where(
+                    actions.T[self.action_space.MOVE] == self.action_space.MOVE.NONE
+                )[0],
+                np.where(
+                    actions.T[self.action_space.TURN] == self.action_space.TURN.LEFT
+                )[0]
+            )).size:
             self.agent_orientations[i_lefters] -= 1
             self.agent_orientations[i_lefters] %= self.SYMMETRY_ORDER
 
-        if 0 < (i_righters := np.where(
-            actions.T[self.action_space.MOVE] == self.action_space.MOVE.NONE and
-            actions.T[self.action_space.TURN] == self.action_space.TURN.RIGHT
-        )[0]).size:
+        if 0 < (i_righters := np.intersect1d(
+                np.where(
+                    actions.T[self.action_space.MOVE] == self.action_space.MOVE.NONE
+                )[0],
+                np.where(
+                    actions.T[self.action_space.TURN] == self.action_space.TURN.RIGHT
+                )[0]
+            )).size:
             self.agent_orientations[i_righters] += 1
             self.agent_orientations[i_righters] %= self.SYMMETRY_ORDER
