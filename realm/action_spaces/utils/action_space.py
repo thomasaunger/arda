76242a931e945dbecbd0db8e0ed210555532165d
Thomas Unger <thomas.a.unger@icloud.com>
2024-05-26 18:55:41 +0200

    Rename variable

--- realm/action_spaces/utils/action_space.py
+++ realm/action_spaces/utils/action_space.py
@@ -10,12 +10,12 @@ class ActionSpace:
         assert isinstance(action_types, dict)
 
         class_attributes = {}
-        for i, (action_type, q) in enumerate(action_types.items()):
+        for i, (action_type, actions) in enumerate(action_types.items()):
             assert isinstance(action_type, str)
             ACTION_TYPE = action_type.upper()
             _ACTION_TYPE = f"_{ACTION_TYPE}"
 
             class_attributes[ACTION_TYPE]  = property(lambda self, _ACTION_TYPE=_ACTION_TYPE: getattr(self, _ACTION_TYPE))
-            class_attributes[_ACTION_TYPE] = ActionType(action_type, i, q)
+            class_attributes[_ACTION_TYPE] = ActionType(action_type, i, actions)
 
         return type(f"ActionSpace{action_space}", (gym.spaces.MultiDiscrete,), class_attributes)(tuple(len(action_type) for action_type in action_types.values()))
